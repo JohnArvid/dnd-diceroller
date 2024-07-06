1440906e6e9a3a14806a5a0890b591d228ea164b
Arvid Lindström <arvid-lindstrom@live.se>
2024-07-06 19:16:28 +0200

    remove clutter from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import Diceroller from './components/diceroller/diceroller.component'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <header>
     <h1>DND</h1>
     <h2>DICEROLLER</h2>
     </header>
-    <p>{count}</p>
-    <button onClick={() => setCount(count + 1)}></button>
     <Diceroller />
     </>
   )
