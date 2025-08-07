83d3b4432e60cd6fe139551b6193f7dd9ae8d3cc
JohnArvid <arvid-lindstrom@live.se>
2025-08-07 08:45:12 +0200

    Fix typo and rounding in rolldice.ts
    Small typo fixed in comment and rounding updated from floor to ceil in the return statement.

--- src/utilities/rolldice.ts
+++ src/utilities/rolldice.ts
@@ -2,11 +2,11 @@ import { DiceType } from "../interfaces/interfaces";
 
 export default function rollDice(dice:DiceType) {
   let max = +dice.slice(1);
-  return (Math.floor(Math.random() * max))
+  return (Math.ceil(Math.random() * max))
 }
 
 /**
  * Är detta en bra allround-funktion för att slå tärning?
- * Funar den lätt att applicera på hur Roll-interfacet ser ut?
+ * Funkar den lätt att applicera på hur Roll-interfacet ser ut?
  */
 
