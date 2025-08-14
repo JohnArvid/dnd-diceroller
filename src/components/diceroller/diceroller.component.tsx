74fb11a62db947f9228fc8460404a5b265a38985
JohnArvid <arvid-lindstrom@live.se>
2025-08-14 09:12:33 +0200

    feat(diceroller): använd state för pickedRoll i diceroller

--- src/components/diceroller/diceroller.component.tsx
+++ src/components/diceroller/diceroller.component.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './diceroller.styles.css';
 
 import Results from '../results/results.component';
 import DicePicker from '../dicepicker/dicepicker.component';
 import PickerMenu from '../pickermenu/pickermenu.component';
-
+import { presetRolls } from '../../utilities/presetRolls';
 import { DicerollerProps } from '../../interfaces/interfaces';
 
 
 
-
 const Diceroller: React.FC<DicerollerProps> = ({}) => {
+  let [pickedRoll, setPickedRoll] = useState(presetRolls[0]);
   return (
     <>
       <Results 
@@ -19,7 +19,7 @@ const Diceroller: React.FC<DicerollerProps> = ({}) => {
         />
       <div className='pickers'>
       <DicePicker />
-      <PickerMenu />
+      <PickerMenu pickedRoll={pickedRoll} setPickedRoll={setPickedRoll}/>
       </div>
     </>
   );
