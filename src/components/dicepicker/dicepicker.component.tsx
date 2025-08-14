a998a50127325c310c86abf420fbe6a224ed9649
JohnArvid <arvid-lindstrom@live.se>
2025-08-14 14:41:27 +0200

    docs(dicepicker): kommentarer om framtida funktioner

--- src/components/dicepicker/dicepicker.component.tsx
+++ src/components/dicepicker/dicepicker.component.tsx
@@ -6,6 +6,23 @@ import Actionbutton from '../actionbutton/actionbutton.component';
 
 const diceTypes:string[] = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20']
 
+/**
+ * När en tärning incrementas första gången skapas ett roll-objekt med den enda tärningen
+ * Och standardfunktionen för att slå tärning samt namnet "Quick roll" 
+ * Det objektet sätts som pickedRoll <- setPickedRoll behöver importeras.
+ * Objektet fylls på allteftersom man lägger till tärningar och modifier
+ * Objektet raderas om man decrementar så det är noll tärningar eller klickar på clear roll
+ * 
+ * ActionButton i Rollpicker visar 'Quick roll' om man inte sparat roll-objektet
+ * 
+ * Hur hantera namngivningen av roll? popup med overlay? 
+ * 
+ * Menyn Custom rolls dyker upp först när det finns något sparat i localStorage.
+ * 
+ * 
+ */
+
+
 const DicePicker: React.FC = () => {
   return (
     <div className='dice-picker'>
@@ -26,6 +43,9 @@ const DicePicker: React.FC = () => {
       <Actionbutton 
       action={() => console.log('Roll saved')}
       text = {'Save roll'} />
+      <Actionbutton 
+      action={() => console.log('Roll cleared')}
+      text = {'Clear roll'} />
     </div>
 
   );
