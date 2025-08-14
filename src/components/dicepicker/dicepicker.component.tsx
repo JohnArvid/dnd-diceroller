125192e0332e50ce6c58615d6ff085fef0dc1141
JohnArvid <arvid-lindstrom@live.se>
2025-08-14 14:30:31 +0200

    feat(dicepicker): skapa array med dicetypes, mappa över den för att skapa Dice-items lägg tillinput för modifier

--- src/components/dicepicker/dicepicker.component.tsx
+++ src/components/dicepicker/dicepicker.component.tsx
@@ -4,27 +4,30 @@ import './dicepicker.styles.css';
 import Dice from '../dice/dice.component'
 import Actionbutton from '../actionbutton/actionbutton.component';
 
-interface DicePickerProps {
+const diceTypes:string[] = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20']
 
-}
-
-const DicePicker: React.FC<DicePickerProps> = () => {
+const DicePicker: React.FC = () => {
   return (
     <div className='dice-picker'>
-     <Dice 
-      diceType='d20'
-      incrementDice={(e)=>(console.log(e.target, ' incremented'))}
-      decrementDice={(e)=>(console.log(e.target, ' decremented'))}
-     />
-     <Dice 
-      diceType='d12'
-      incrementDice={(e)=>(console.log(e.target, ' incremented'))}
-      decrementDice={(e)=>(console.log(e.target, ' decremented'))}
-     />
+      {diceTypes.map((diceType) => {
+        return (
+        <Dice diceType={diceType} 
+        incrementDice={(e)=>(console.log(e.target, diceType + ' incremented'))}
+        decrementDice={(e)=>(console.log(e.target, diceType + ' decremented'))}
+        key={diceType}
+        />
+      )
+      })}
+      <div>
+        <label htmlFor="modifier">Modifier:</label>
+        <input id='modifier' type='text' size={4}></input>
+      </div>
+        
       <Actionbutton 
       action={() => console.log('Roll saved')}
       text = {'Save roll'} />
     </div>
+
   );
 };
 
