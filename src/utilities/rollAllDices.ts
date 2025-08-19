c34714bc0f61c2ff7af6234ab777f11be1ddb34c
JohnArvid <arvid-lindstrom@live.se>
2025-08-19 08:57:49 +0200

    refactor(rollAllDices): Ge explicita typer till parametrar

--- src/utilities/rollAllDices.ts
+++ src/utilities/rollAllDices.ts
@@ -1,8 +1,8 @@
 import { diceObj } from "../interfaces/interfaces";
 import rollDice from "./rolldice";
 
-export default function rollAllDices(arrayOfDiceObj, modifier):number {
-      let result = arrayOfDiceObj.dice.reduce((sum:number, current:diceObj) => {
+export default function rollAllDices(arrayOfDiceObj:diceObj[], modifier:number):number {
+      let result = arrayOfDiceObj.reduce((sum:number, current:diceObj) => {
         let currentResult = 0;
         for(let i = 0; i < current.qnt; i++) {
           currentResult += rollDice(current.diceType);
