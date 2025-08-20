1717aae6b597bcc7be9cd255d07ddabda329b2b0
JohnArvid <arvid-lindstrom@live.se>
2025-08-20 09:08:12 +0200

    feat(Dice): lägg til data-attribut på dice komponent så att man enkelt kommer åt dicetype

--- src/components/dice/dice.component.tsx
+++ src/components/dice/dice.component.tsx
@@ -8,7 +8,7 @@ const Dice: React.FC<DiceProps> = ({
   decrementDice,
 }) => {
   return (
-    <div className='dice'>
+    <div className='dice' data-diceType={diceType}>
       <button onClick={decrementDice}>-</button>
       <span>{diceType}</span>
       <button onClick={incrementDice}>+</button>
